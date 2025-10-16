97dc38e0afbd5885bd417dea7c20c592912cfd68
sbhavanibhumana_cse230557 <sbhavanibhumana_cse230557@mgit.ac.in>
2025-10-16 16:24:13 +0000

    c-2

--- backend/app/pdf_ingest.py
+++ backend/app/pdf_ingest.py
@@ -1,8 +1,6 @@
 # backend/app/pdf_ingest.py
 import os
 from dotenv import load_dotenv # type: ignore
-from langchain_community.document_loaders import PyPDFLoader # type: ignore
-
 load_dotenv()
 
 S3_BUCKET = os.getenv("S3_BUCKET")  # optional
@@ -28,11 +26,3 @@ def upload_to_s3(local_path: str, key: str):
     s3.upload_file(local_path, S3_BUCKET, key)
     # return https URL (or s3:// URI)
     return f"https://{S3_BUCKET}.s3.amazonaws.com/{key}"
-
-def process_pdf(file_path: str):
-    """
-    Processes a PDF file and returns the loaded documents.
-    """
-    loader = PyPDFLoader(file_path)
-    documents = loader.load()
-    return documents
